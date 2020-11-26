d2337cc0101725c8f1404dc4639d6e48569d5966
Piotr Franczak <petetrainerlsf@gmail.com>
2020-11-26 17:54:21 +0100

    added heading component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,7 +4,7 @@ import {withRouter} from "react-router-dom"
 
 const MenuItem = ({title, imageUrl, size, linkUrl, match, history}) =>{
     return(
-    <div className={`${size} menu-item`} onClick={()=>history.push(`{match.url}${linkUrl}`)} >
+    <div className={`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)} >
     <div className="background-image" 
     style={{backgroundImage: `url(${imageUrl})`}} />
                  <div className="content">
