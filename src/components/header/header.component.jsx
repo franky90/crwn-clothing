d6b516e4d682b8a8eb7d2c1926a466ae5bde7717
Piotr Franczak <petetrainerlsf@gmail.com>
2020-11-26 19:26:39 +0100

    added form page

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -14,7 +14,7 @@ const Header = () => {
         <Link className="option" to="/shop">
         SHOP 
         </Link>
-        <Link className="option" to="/contact">
+        <Link className="option" to="/contact"> 
         CONTACT
         </Link>
         
